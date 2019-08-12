560837c1965e10e75145c395f4f075a9d6644aa9
frasq <frasq@so-o.org>
2019-08-12 17:03:59 +0200

    Edits testCard.php

--- poker/testCard.php
+++ poker/testCard.php
@@ -2,8 +2,8 @@
 
 /**
  *
- * @copyright  2012 so-o.org
- * @version    1
+ * @copyright  2012-2019 so-o.org
+ * @version    2
  * @link       http://www.so-o.org
  */
 
@@ -17,5 +17,5 @@ $card_2C=sendmsg($Card, 'new', \Card\TWO, \Card\CLUBS);
 echo '2c (two of clubs) -> ';
 sendmsg($card_2C, 'print', true);
 $card_Kh=sendmsg($Card, 'new', \Card\KING, \Card\HEARTS);
-echo 'Kh (king of hearts -> ';
+echo 'Kh (king of hearts) -> ';
 sendmsg($card_Kh, 'print', true);
